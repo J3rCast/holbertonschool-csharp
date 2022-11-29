6e98dbdd53394c2ed297dcd3114ecadb01ff6530
J3rCast <jerson.castro.r@gmail.com>
2022-11-29 16:31:18 -0500

    task 1 fixed

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -7,15 +7,9 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
         int LastDigit = number % 10;
-        string SubString = "";
 
-        if (LastDigit > 5)
-            SubString = "and is greater than 5";
-        else if (LastDigit == 0)
-            SubString = "and is 0";
-        else if (LastDigit < 6)
-            SubString = "and is less than 6 and not 0";
-
-        Console.WriteLine("The last digit of {0} is {1} {2}", number, LastDigit, SubString);
+        Console.WriteLine("The last digit of {0} is {1} {2}", number, LastDigit, LastDigit > 5
+        ? "and is greater than 5" : (LastDigit == 0 ? "and is 0" 
+        : "and is less than 6 and not 0"));
     }
 }
