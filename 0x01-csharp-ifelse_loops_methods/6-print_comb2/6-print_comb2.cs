ee5e69a0c484cee13efa1ff45da5ffd550f2163e
J3rCast <jerson.castro.r@gmail.com>
2022-11-29 17:36:46 -0500

    task 6 added

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 1;
+            for (i = 0; i < 10 ; i++)
+            {
+                while (j < 10)
+                {
+                    if (i == 8 && j == 9)
+                        Console.Write("{0}{1}\n", i, j);
+                    else if (i != j)
+                        Console.Write("{0}{1}, ", i, j);
+                    j++;
+                }
+                j = i + 1;
+            }
+        }
+    }
+}
