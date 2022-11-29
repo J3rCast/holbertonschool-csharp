6386f5402fe18651a89999425dfc952b5ee81fb2
J3rCast <jerson.castro.r@gmail.com>
2022-11-29 17:07:30 -0500

    task 5 added

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[100];
+
+            for (int i = 0; i <= 99; i++)
+                array.SetValue(value: i, index: i);
+
+            Console.WriteLine("{0}", String.Join(", ", array));
+        }
+    }
+}
