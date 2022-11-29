9a968ce3ad3a78bb1a29e7f88ff3ab078206c543
J3rCast <jerson.castro.r@gmail.com>
2022-11-29 18:20:23 -0500

    task 11 added

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int j = 0;
+
+        if (length <= 0)
+            Console.WriteLine();
+
+        for (int i = 0; i < length; i++)
+        {
+            while (j < i)
+            {
+                Console.Write(" ");
+                j++;
+            }
+            j = 0;
+
+            Console.WriteLine("\\");
+        }
+    }
+}
