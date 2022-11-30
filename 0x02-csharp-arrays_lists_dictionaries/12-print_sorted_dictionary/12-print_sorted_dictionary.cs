46208100eceb6b3b24046788922948ff8a02e2e1
J3rCast <jerson.castro.r@gmail.com>
2022-11-30 12:24:24 -0500

    task 12 and 13 added

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedDict = from entry in myDict orderby entry.Key ascending select entry;
+
+        foreach (var item in sortedDict)
+        {
+            Console.WriteLine("{0}: {1}", item.Key, item.Value);
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = -1;
+        string maxScore = "";
+
+        if (myList.Count == 0 || myList == null)
+            return "None";
+
+        foreach (var item in myList)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+                maxScore = item.Key;
+            }
+        }
+
+        return maxScore;
+    }
+}
