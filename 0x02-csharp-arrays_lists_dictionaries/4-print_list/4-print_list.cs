2ffc209a394ba363a5db9870414ce7727812fe5f
J3rCast <jerson.castro.r@gmail.com>
2022-11-30 09:20:06 -0500

    task 4 added

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+            Console.WriteLine("");
+
+        List<int> list = new List<int>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            if (i < size - 1)
+                Console.Write("{0} ", i);
+            else
+                Console.WriteLine("{0}", i);
+        }
+        return list;
+    }
+}
