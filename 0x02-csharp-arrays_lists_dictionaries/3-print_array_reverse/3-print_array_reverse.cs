0e505f284514b8095d1ff1b4b294f8da416c6e6f
J3rCast <jerson.castro.r@gmail.com>
2022-11-30 09:11:21 -0500

    task 3 added

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+        int i = array.Length;
+        for (i = array.Length - 1; i >= 1; i--)
+            Console.Write("{0} ", array[i]);
+
+        Console.Write("{0}\n", array[i]);
+    }
+}
