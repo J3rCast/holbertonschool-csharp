99a251ebad2542edc202d019c6063f46a79e3348
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 12:33:43 -0500

    task 1 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Sorting
+{
+    public static List<int> GetSortedList(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i] > list[j])
+                {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        return list;
+    }
+}
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list2.Contains(list1[i]) && result.Contains(list1[i]) == false)
+                result.Add(list1[i]);
+        }
+        return Sorting.GetSortedList(result);
+    }
+}
