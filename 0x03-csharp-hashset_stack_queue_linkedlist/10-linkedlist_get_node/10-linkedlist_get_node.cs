620c3006a1d11cec65d7b229298e8e948ee46550
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 15:16:16 -0500

    task 9 added

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
+        if (myLList.Count == 0)
+            return 0;
+
+        LinkedListNode<int> node = myLList.First;
+
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (i == n)
+                return node.Value;
+            node = node.Next;
+        }
+        return 0;
     }
 }
