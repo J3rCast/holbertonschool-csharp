f7ea5e9d63650284bc17b55ee82e4c7dc61ee357
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 11:44:28 -0500

    task 0 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> result = new HashSet<int>(myList);
+        int sum = 0;
+
+        foreach (var item in result)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
