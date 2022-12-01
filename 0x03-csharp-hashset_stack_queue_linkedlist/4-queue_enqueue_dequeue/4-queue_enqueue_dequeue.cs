c071b5957e4884bdd8e7bbb8844a605129a02380
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 14:14:12 -0500

    task 4 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        List<string> Copy = new List<string>(aQueue);
+        if (aQueue.Contains(search))
+            for (int i = 0; i < Copy.Count; i++)
+            {
+                aQueue.Dequeue();
+                if (Copy[i] == search)
+                    break;
+            }
+        return aQueue;
+    }
+}
