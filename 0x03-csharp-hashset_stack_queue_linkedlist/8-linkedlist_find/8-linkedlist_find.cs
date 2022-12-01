7727162dcc417ae2f12d79a690b937592c916e2a
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 15:02:15 -0500

    task 8 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        LinkedListNode<int> node = myLList.First;
+
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            if (node.Value == value)
+                return i;
+            node = node.Next;
+        }
+        return -1;
+    }
+}
