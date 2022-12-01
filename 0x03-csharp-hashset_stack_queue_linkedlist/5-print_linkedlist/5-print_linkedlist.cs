010265b2b781085eb4af30d5f7aec0e5b2208adc
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 14:39:22 -0500

    task 5 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> result = new LinkedList<int>();
+
+        if (size < 0)
+            return result;
+
+
+        for (int i = 0; i < size; i++)
+        {
+            result.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return result;
+    }
+}
