93b2a188ba16bbfff86c0d4aecacafb0870f7199
J3rCast <jerson.castro.r@gmail.com>
2022-12-01 14:47:45 -0500

    task 6 added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int Count = 0;
+        foreach (var item in myLList)
+        {
+            Count++;
+        }
+        return Count;
+    }
+}
