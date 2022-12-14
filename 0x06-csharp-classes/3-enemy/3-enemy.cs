40e5040e594e20914fa685b96ab4380d1460426b
J3rCast <jerson.castro.r@gmail.com>
2022-12-14 14:52:48 -0500

    task 3 added

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace _3_enemy
+namespace Enemies
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	/// <summary>
+	/// Empty class with a default constructor
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>New health field</summary>
+		public int health;
+
+		/// <summary>Initializes a new instances of the <see cref="Zombie"/> class.</summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+
+		/// <summary>Initializes a new instance of the <see cref="Zombie"/> class
+		/// allowing using arguments.</summary>
+		public Zombie(int value)
+		{
+			if (value < 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			health = value;
+		}
+
+		/// <summary>Get health of the <see cref="Zombie"/>.</summary>
+		/// <returns>The health of the <see cref="Zombie"/>.</returns>
+		public int GetHealth()
+		{
+			return health;
+		}
+	}
 }
