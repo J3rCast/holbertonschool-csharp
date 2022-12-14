976d71d40f66b279ae53155a8a7f3d16957c2fd2
J3rCast <jerson.castro.r@gmail.com>
2022-12-14 15:07:40 -0500

    task 5 added

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace _5_enemy
+namespace Enemies
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	/// <summary>
+	/// Empty class with a default constructor
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>Fields.</summary>
+		public int health;
+		private string name = "(No name)";
+
+		/// <summary>Initializes a new instances of the <see cref="Zombie"/> class.</summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+
+		/// <summary>Initializes a new instance of the <see cref="Zombie"/> class
+		/// allowing using arguments.</summary>
+		public Zombie(int value)
+		{
+			if (value < 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			health = value;
+		}
+
+		/// <summary>Get health of the <see cref="Zombie"/>.</summary>
+		/// <returns>The health of the <see cref="Zombie"/>.</returns>
+		public int GetHealth()
+		{
+			return health;
+		}
+
+		/// <summary>Propery for name.</summary>
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		/// <summary>Overrides ToString method.</summary>
+		public override string ToString() => $"Zombie Name: {name} / Total Health: {health}";
+	}
 }
