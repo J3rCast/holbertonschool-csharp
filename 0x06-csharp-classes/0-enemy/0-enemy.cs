98d75c4a0f398efc0ecb201f3585dd5e2a8cf204
J3rCast <jerson.castro.r@gmail.com>
2022-12-14 14:23:26 -0500

    task 0 and 1 added

---
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Enemies
+{
+	/// <summary>
+	/// This is an empty class
+	/// </summary>
+	public class Zombie
+	{
+	}
+}
---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+	/// <summary>
+	/// Empty class with a default constructor
+	/// </summary>
+	public class Zombie
+	{
+		public int health;
+
+		public Zombie()
+		{
+			health = 0;
+		}
+	}
+}
