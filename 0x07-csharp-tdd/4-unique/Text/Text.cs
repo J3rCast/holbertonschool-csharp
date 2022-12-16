9d51edc7b11c1bde5cfccb77b0dcb6c2114336bf
J3rCast <jerson.castro.r@gmail.com>
2022-12-16 13:58:28 -0500

    task 4 added

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Text
+{
+    public class Str
+    {
+		public static int UniqueChar(string s)
+		{
+			List<char> CharArray = s.ToList();
+			List<char> Removed = new List<char>();
+			char temp = '\0';
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				temp = s[i];
+				CharArray.RemoveAt(0);
+
+				if (!Removed.Contains(temp) && !CharArray.Contains(temp))
+					return i;
+
+				Removed.Add(temp);
+			}
+			return -1;
+		}
+    }
+}
