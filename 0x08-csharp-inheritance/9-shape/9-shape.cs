4ee85426f8acdb797ea9d03fc689fc76c4e21a91
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 17:09:44 -0500

    task 9 added

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+/// <summary>This class contains multiple methods related to shape.</summary>
+class Shape
+{
+	/// <summary>This method throws an exception.</summary>
+	public virtual int Area()
+	{
+		throw new NotImplementedException("Area() is not implemented");
+	}
+}
+
+/// <summary>class Rectangle that inherits from Shape.</summary>
+class Rectangle : Shape
+{
+	private int width;
+	private int height;
+
+	public int Width
+	{
+		get { return width; }
+		set { width = value < 0 ? throw new ArgumentException("Width must be greater than or equal to 0") : value;}
+	}
+
+	public int Height
+	{
+		get { return height; }
+		set { height = value < 0 ? throw new ArgumentException("Height must be greater than or equal to 0") : value;}
+	}
+
+	/// <summary>This method overrides Area method of Shape.</summary>
+	public new int Area()
+	{
+		return width * height;
+	}
+
+	/// <summary>This method overrides ToString method.</summary>
+	public override string ToString()
+	{
+		return $"[Rectangle] {width} / {height}";
+	}
+}
+
+/// <summary>class Square that inherits from Shape.</summary>
+class Square : Rectangle
+{
+	private int size;
+
+	public int Size
+	{
+		get { return size; }
+		set 
+		{
+			if (value < 0)
+				throw new ArgumentException("Size must be greater than or equal to 0");
+
+			size = value;
+			this.Width = size;
+			this.Height = size;
+		}
+	}
+
+	/// <summary>This method overrides ToString method.</summary>
+	public override string ToString()
+	{
+		return $"[Square] {size} / {size}";
+	}
+}
