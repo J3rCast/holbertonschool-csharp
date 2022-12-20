728674df50caac53188b560b4044abd743de8a47
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 16:13:33 -0500

    task 5 added

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>This class contains multiple methods related to shape.</summary>
+class Shape
+{
+	/// <summary>This method throws an exception.</summary>
+	public virtual int Area()
+	{
+		throw new NotImplementedException("Area() is not implemented");
+	}
+}
