df374e19f29009b5cb8d01f8bf9960539a7b6e35
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 13:12:45 -0500

    task 0 added

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -3,7 +3,7 @@
 /// <summary>Contains multiple methods for objects </summary>
 class Obj
 {
-	/// <summary>check if <see cref="object" /> is int. </summary>
+	/// <summary>check if obj is int. </summary>
 	/// <returns>true if obj is int. </returns>
     public static bool IsOfTypeInt(object obj)
     {
