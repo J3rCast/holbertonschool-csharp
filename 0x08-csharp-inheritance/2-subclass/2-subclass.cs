4f281d10c5139bdeed14b940a204ad1e0d54049e
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 14:13:59 -0500

    task 2 added

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -3,7 +3,7 @@
 /// <summary>Contains multiple methods for Obj.</summary>
 class Obj
 {
-	/// <summary>check if <see cref="derivedType"/> is a subclass of <see cref="baseType"/>.</summary>
+	/// <summary>check if derivedType is a subclass of baseType.</summary>
 	/// <returns>True if is a subclass.</returns>
 	public static bool IsOnlyASubclass(Type derivedType, Type baseType)
 	{
