229528f4d0b7560dd67cb979f1762b6cfec4421d
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 15:00:34 -0500

    task 3 added

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+
+/// <summary>Contains multiple methods for Obj.</summary>
+class Obj
+{
+	/// <summary>Print object info.</summary>
+	public static void Print(object myObj)
+	{
+		TypeInfo t = myObj.GetType().GetTypeInfo();
+		IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+		IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+		Console.WriteLine(myObj.GetType().Name + " Properties:");
+		Console.WriteLine(myObj.GetType().Name + " Methods:");
+
+		foreach (PropertyInfo p in pList)
+		{
+			Console.WriteLine(p.Name);
+		}
+
+		foreach (MethodInfo m in mList)
+		{
+			Console.WriteLine(m.Name);
+		}
+
+		Console.WriteLine(t.GetTypeInfo());
+	}
+}
