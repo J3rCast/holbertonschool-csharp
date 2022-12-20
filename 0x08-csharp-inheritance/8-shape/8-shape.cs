fddc0ccb783e03ff1745e06e3005436f67e8096a
J3rCast <jerson.castro.r@gmail.com>
2022-12-20 17:00:26 -0500

    task 8 added

--- 0x08-csharp-inheritance/8-shape/8-shape.cs
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
+/// <summary>This class contains multiple methods related to shape.</summary>
 class Shape
 {
+	/// <summary>This method throws an exception.</summary>
 	public virtual int Area()
 	{
 		throw new NotImplementedException("Area() is not implemented");
 	}
 }
 
+/// <summary>class Rectangle that inherits from Shape.</summary>
 class Rectangle : Shape
 {
 	private int width;
@@ -25,13 +28,35 @@ class Rectangle : Shape
 		set { height = value < 0 ? throw new ArgumentException("Height must be greater than or equal to 0") : value;}
 	}
 
+	/// <summary>This method overrides Area method of Shape.</summary>
 	public new int Area()
 	{
 		return width * height;
 	}
 
+	/// <summary>This method overrides ToString method.</summary>
 	public override string ToString()
 	{
 		return $"[Rectangle] {width} / {height}";
+	}
+}
+
+/// <summary>class Square that inherits from Shape.</summary>
+class Square : Rectangle
+{
+	private int size;
+
+	public int Size
+	{
+		get { return size; }
+		set 
+		{
+			if (value < 0)
+				throw new ArgumentException("Size must be greater than or equal to 0");
+
+			size = value;
+			this.Width = size;
+			this.Height = size;
+		}
 	}
 }
