d2d224ed0d59c0913d06ab0ce236762fa3378c36
J3rCast <jerson.castro.r@gmail.com>
2023-01-10 15:28:21 -0500

    task 3 added

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
+            return Math.Round(Math.Sqrt(res), 2);
+        }
+        if (vector.Length == 3)
+        {
+            double res = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2);
+            return Math.Round(Math.Sqrt(res), 2);
+        }
+        
+        return -1;
+    }
+}
+
