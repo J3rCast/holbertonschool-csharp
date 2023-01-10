600b2605a8534539a7f516e1d657505c6d6df44f
J3rCast <jerson.castro.r@gmail.com>
2023-01-10 18:04:29 -0500

    task 6 added

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != vector2.Length)
+        {
+            double[] fail = {-1};
+            return fail;
+        }
+        
+        double[] res = vector1;
+
+        if (vector1.Length == 2 || vector1.Length == 3)
+        {
+            for (int i = 0 ; i < vector1.Length ; i++)
+                res[i] = vector1[i] + vector2[i];
+        }
+        return res;
+    }
+}
