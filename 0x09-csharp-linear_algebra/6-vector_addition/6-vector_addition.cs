580ded94ae3ce8b5ddf3b5cac5aa81b3d4e24b86
J3rCast <jerson.castro.r@gmail.com>
2023-01-10 18:17:48 -0500

    task 6 added

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -17,10 +17,11 @@ class VectorMath
         if (vector1.Length != vector2.Length) 
             return fail;
         
-        double[] res = vector1;
+        
 
         if ((vector1.Length == 2 || vector1.Length == 3) && (vector2.Length == 2 || vector2.Length == 3))
         {
+            double[] res = new double[vector1.Length];
             for (int i = 0 ; i < vector1.Length ; i++)
                 res[i] = vector1[i] + vector2[i];
             
