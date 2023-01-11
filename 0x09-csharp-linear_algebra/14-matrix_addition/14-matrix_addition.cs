2f4fa4ae07839ce7a8a9b36bafe64ef0ce874cb0
J3rCast <jerson.castro.r@gmail.com>
2023-01-11 17:42:49 -0500

    task 14 added

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -9,10 +9,13 @@ class MatrixMath
         if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
 			return fail;
 
+		// check if the high is 2 or 3, return fail otherwise.
 		if (matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3)
 			return fail;
+		// check if the witdh is 2 or 3, return fail otherwise.
 		if (matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3)
 			return fail;
+		// check if the high and the witdh are the same, return fail otherwise.
 		if (matrix1.GetLength(0) != matrix1.GetLength(1))
 			return fail;
 
