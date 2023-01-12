213ca381d3c55d9aa6ba393bb066f917a421d389
J3rCast <jerson.castro.r@gmail.com>
2023-01-12 16:02:51 -0500

    task 25 added

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+		double[,] res = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int row = 0; row < res.GetLength(0); row++)
+		{
+			for (int col = 0; col < res.GetLength(1); col++)
+			{
+				res[row, col] = matrix[col, row];
+			}
+		}
+		return res;
+    }
+}
+
