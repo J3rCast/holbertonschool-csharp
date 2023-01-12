5861422b1d9ab061e8f508688febc8c2dce1a8cc
J3rCast <jerson.castro.r@gmail.com>
2023-01-12 18:24:46 -0500

    task 30 added

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -12,7 +12,7 @@ class VectorMath
 		double[] res = new double[3];
 
 		res[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
-		res[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+		res[1] = -1 * (vector1[0] * vector2[2]) - (vector1[2] * vector2[0]);
 		res[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
 
 		return res;
