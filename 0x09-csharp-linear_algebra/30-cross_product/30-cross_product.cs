2ec616c0fcb18dbe2986c99b3d6b609a34405d95
J3rCast <jerson.castro.r@gmail.com>
2023-01-12 18:12:32 -0500

    task 30 added

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] fail = {-1};
+
+		if (vector1.Length != 3 || vector2.Length != 3)
+			return fail;
+
+		double[] res = new double[3];
+
+		res[0] = Math.Round((vector1[1] * vector2[2]) - (vector1[2] * vector2[1]));
+		res[1] = Math.Round((vector1[0] * vector2[2]) - (vector1[2] * vector2[0]));
+		res[2] = Math.Round((vector1[0] * vector2[1]) - (vector1[1] * vector2[0]));
+
+		return res;
+    }
+}
