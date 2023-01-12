5ebbcfd4f4c29335aa9801f3666361c32f591e1d
J3rCast <jerson.castro.r@gmail.com>
2023-01-12 14:48:05 -0500

    task 24 added

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+		double[,] fail = {{-1}};
+
+		if (matrix.Length != 4 || (direction != 'x' && direction != 'y'))
+			return fail;
+
+		double[,] res = new double[2, 2];
+
+		if (direction == 'x')
+		{
+
+			res[0, 0] = matrix[0, 0] + factor * matrix[0, 1];
+			res[1, 0] = matrix[1, 0] + factor * matrix[1, 1];
+			res[0, 1] = matrix[0, 1];
+			res[1, 1] = matrix[1, 1];
+		}
+		if (direction == 'y')
+		{
+			res[0, 1] = matrix[0, 1] + factor * matrix[0, 0];
+			res[1, 1] = matrix[1, 1] + factor * matrix[1, 0];
+			res[0, 0] = matrix[0, 0];
+			res[1, 0] = matrix[1, 0];
+		}
+		return res;
+    }
+}
